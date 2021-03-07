e1706997b598b55b35c3f854f4705bbd0566149c
insookim0702 <insookim0702@gmail.com>
2021-03-07 19:42:59 +0900

    버튼 누르면 로딩 모달 나오는 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
+const photoCard = document.querySelector('.photoCard');
+const loading = document.querySelector('.loading');
+
+function handleClickPhotoAlbum(event) {
+    loading.classList.replace('displayNone', 'displayGrid');
+}
+
 function init() {
-    const w = '1';
+    photoCard.addEventListener('click', handleClickPhotoAlbum);
 }
 
 init();
